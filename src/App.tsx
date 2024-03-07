4c53048a4a3ecc751534f11fcf7b99cd76f58028
Roxpj <148432952+Roxpj@users.noreply.github.com>
2024-03-07 21:15:01 +0100

    added public files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import  { useState } from 'react';
-import NavbarComponent from './components/Navbar.jsx';
-
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <NavbarComponent />
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
